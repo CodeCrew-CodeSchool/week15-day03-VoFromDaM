bd8db5af4a303c7acdf923dc7f0b4c3583e35451
VoFromDaM <shaunsmart999@gmail.com>
2024-06-06 14:03:19 -0500

    Assignment

---
+++ MyApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Tic-Tac-Toe!");
+
+        Console.Write("Enter name for Player 1: ");
+        string player1Name = Console.ReadLine();
+        Player player1 = new Player(player1Name, "X");
+
+        Console.Write("Enter name for Player 2: ");
+        string player2Name = Console.ReadLine();
+        Player player2 = new Player(player2Name, "O");
+
+        Game game = new Game(player1, player2);
+        game.Play();
+
+        Console.WriteLine("Thanks for playing!");
+    }
+}
